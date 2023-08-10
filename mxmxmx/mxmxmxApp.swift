f4e9cb4bf2e99ed210497e82cde21bb0c93e935a
Jianna Correia Silva <keilajianna@gmail.com>
2023-08-10 13:34:05 -0400

    Initial Commit

---
+++ mxmxmx/mxmxmxApp.swift
@@ -0,0 +1,17 @@
+//
+//  mxmxmxApp.swift
+//  mxmxmx
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mxmxmxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
